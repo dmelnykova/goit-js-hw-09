2648e977e3d227f243eca9d82c59db2811a17f49
dmelnykova <130089251+dmelnykova@users.noreply.github.com>
2023-10-05 17:22:16 +0000

    Deploying to gh-pages from @ dmelnykova/goit-js-hw-09@9b3c0adbc4bb91d300dfb260dbe96f01b9256e6a 🚀

---
+++ 01-color-switcher.5997423f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");let a;function n(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,a=setInterval(n,1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.5997423f.js.map
