4215643c5d8d91c4c324e45a8e4d1acf475002d5
dariameln1 <dariamelnykova1@gmail.com>
2023-10-06 15:33:54 +0200

    task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,49 +1,55 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { convertMs } from "../plus/convert-time-02";
 
-const datetimePicker = document.getElementById("datetime-picker");
-const startButton = document.querySelector("[data-start]");
+const datetimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const timerElements = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]')
+};
+
+startButton.disabled = true;
+let intervalId;
 
 const options = {
   enableTime: true,
   time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    if (selectedDate < new Date()) {
-      alert("Please choose a date in the future");
-      startButton.disabled = true;
+    if (selectedDates[0] < Date.now()) {
+      alert('Please choose a date in the future');
     } else {
       startButton.disabled = false;
     }
   },
 };
 
+startButton.addEventListener("click", onStart);
+
+function onStart() {
+  const endDate = new Date(datetimePicker.value);
+
+  if (isNaN(endDate) || endDate <= Date.now()) {
+    alert('Please choose a valid date and time in the future.');
+    return;
+  }
+
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    updateTimer(endDate);
+  }, 1000);
+}
+
 flatpickr(datetimePicker, options);
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
 function addLeadingZero(value) {
-  return value < 10 ? `0${value}` : value;
+  return value < 10 ? `0${value}` : value.toString();
 }
-
-const daysElement = document.querySelector("[data-days]");
-const hoursElement = document.querySelector("[data-hours]");
-const minutesElement = document.querySelector("[data-minutes]");
-const secondsElement = document.querySelector("[data-seconds]");
-
-let intervalId;
 
 function updateTimer(endDate) {
   const currentDate = new Date();
@@ -51,24 +57,16 @@ function updateTimer(endDate) {
 
   if (difference <= 0) {
     clearInterval(intervalId);
-    daysElement.textContent = "00";
-    hoursElement.textContent = "00";
-    minutesElement.textContent = "00";
-    secondsElement.textContent = "00";
     return;
   }
 
   const { days, hours, minutes, seconds } = convertMs(difference);
-  daysElement.textContent = addLeadingZero(days);
-  hoursElement.textContent = addLeadingZero(hours);
-  minutesElement.textContent = addLeadingZero(minutes);
-  secondsElement.textContent = addLeadingZero(seconds);
+  updateTimerDisplay(days, hours, minutes, seconds);
 }
 
-startButton.addEventListener("click", () => {
-  console.log("Button clicked");
-  const endDate = new Date(datetimePicker.value);
-  intervalId = setInterval(() => {
-    updateTimer(endDate);
-  }, 1000);
-});
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  timerElements.days.textContent = addLeadingZero(days);
+  timerElements.hours.textContent = addLeadingZero(hours);
+  timerElements.minutes.textContent = addLeadingZero(minutes);
+  timerElements.seconds.textContent = addLeadingZero(seconds);
+}
