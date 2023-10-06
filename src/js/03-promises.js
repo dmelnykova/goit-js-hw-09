957837a16c228db83bb72e625a18af9dd6313e27
dariameln1 <dariamelnykova1@gmail.com>
2023-10-06 17:22:35 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,13 @@
+import { throttle } from 'lodash';
+import { Notify } from 'notiflix';
+
+const userData = {};
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('input', throttle(handleFormInput, 500));
+formEl.addEventListener('submit', handleFormSubmit);
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -24,12 +34,23 @@ function handleFormSubmit(event) {
     createPromise(i, currentDelay)
       .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
   }
 }
 
-const form = document.querySelector('.form');
-form.addEventListener('submit', handleFormSubmit);
+function handleFormInput(event) {
+  if (event.target.name === 'delay') {
+    userData.delay = parseInt(event.target.value);
+  }
+  if (event.target.name === 'step') {
+    userData.step = parseInt(event.target.value);
+  }
+  if (event.target.name === 'amount') {
+    userData.amount = parseInt(event.target.value);
+  }
+}
